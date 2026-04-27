e47ae9e7d565a5b380ea36b1547080a293ee00eb
udaygarg13 <udaygarg2003@gmail.com>
2026-04-27 16:43:16 -0600

    Android

---
+++ react-client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fullhouse.poker',
+  appName: 'Poker',
+  webDir: 'dist'
+};
+
+export default config;
